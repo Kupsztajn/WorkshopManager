8e4527766abb2fe55c5ffc63c4b132aea9436236
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2025-05-23 21:54:01 +0200

    -Added greeting by name

--- WorkshopManager/Controllers/KlientController.cs
+++ WorkshopManager/Controllers/KlientController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WorkshopManager.Models;
 
 namespace WorkshopManager.Controllers;
 
 public class KlientController : Controller
 {
-    public IActionResult Index()
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public KlientController(UserManager<ApplicationUser> userManager)
     {
+        _userManager = userManager;
+    }
+
+    // GET: /Recepcjonista
+    public async Task<IActionResult> Index()
+    {
+        // User.Identity.Name to domyślnie UserName/email
+        var username = User.Identity.Name;
+        var user = await _userManager.FindByNameAsync(username);
+            
+        // Przekazujemy imię do widoku np. przez ViewBag
+        ViewBag.FirstName = user?.Name;
+            
         return View();
     }
 }
--- WorkshopManager/Controllers/RecepcjonistaController.cs
+++ WorkshopManager/Controllers/RecepcjonistaController.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WorkshopManager.Models;
 
 namespace WorkshopManager.Controllers;
 
 [Authorize(Roles = "Recepcjonista")]
 public class RecepcjonistaController : Controller
 {
-    public IActionResult Index()
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public RecepcjonistaController(UserManager<ApplicationUser> userManager)
     {
+        _userManager = userManager;
+    }
+
+    // GET: /Recepcjonista
+    public async Task<IActionResult> Index()
+    {
+        // User.Identity.Name to domyślnie UserName/email
+        var username = User.Identity.Name;
+        var user = await _userManager.FindByNameAsync(username);
+            
+        // Przekazujemy imię do widoku np. przez ViewBag
+        ViewBag.FirstName = user?.Name;
+            
         return View();
     }
 }
