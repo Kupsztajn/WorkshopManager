62ab4acf9bea22cb8552e2b4c7029bd5fbaf2706
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2025-05-25 00:50:20 +0200

    -US3 is partially done (missing phone number) -Added db.Clients -Receptionist now can add Clients through form -Client is added to db.Client database

--- WorkshopManager/Controllers/RecepcjonistaController.cs
+++ WorkshopManager/Controllers/RecepcjonistaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WorkshopManager.Data;
 using WorkshopManager.Models;
 
 namespace WorkshopManager.Controllers;
@@ -9,10 +10,12 @@ namespace WorkshopManager.Controllers;
 public class RecepcjonistaController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly UsersDbContext _context;
 
-    public RecepcjonistaController(UserManager<ApplicationUser> userManager)
+    public RecepcjonistaController(UserManager<ApplicationUser> userManager, UsersDbContext context)
     {
         _userManager = userManager;
+        _context = context;
     }
 
     // GET: /Recepcjonista
@@ -26,5 +29,41 @@ public class RecepcjonistaController : Controller
         ViewBag.FirstName = user?.Name;
             
         return View();
+    }
+    
+    // GET: Recepcjonista/AddClient
+    public IActionResult AddClient()
+    {
+        return View();
+    }
+
+    // POST: Recepcjonista/AddClient
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> AddClient(Client model)
+    {
+        ModelState.Remove("AddedByUserId");
+        ModelState.Remove("AddedByUser");
+
+        if (!ModelState.IsValid)
+        {
+            // Zbierz wszystkie komunikaty błędów
+            var errors = ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage)
+                .ToList();
+            // Przekaż je do widoku przez ViewBag
+            ViewBag.Errors = errors;
+            return View(model);
+        }
+
+        var user = await _userManager.GetUserAsync(User);
+        model.AddedByUserId = user.Id;
+        
+        _context.Clients.Add(model);
+        await _context.SaveChangesAsync();
+
+        // Po dodaniu klienta możesz przekierować do listy klientów lub panelu
+        return RedirectToAction("Index");
     }
 }
---
+++ WorkshopManager/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkshopManager.Models;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    // inne pola według potrzeb
+    // Relacja do recepcjonisty (użytkownika Identity), który dodał klienta
+    public string AddedByUserId { get; set; }
+    public ApplicationUser AddedByUser { get; set; }
+}
