87cbfe08fbd4d9dbfa4e6908bf477eea9ee7fcc9
Kupsztajn <nawrocki.jakub@gmail.com>
2025-05-31 12:03:40 +0200

    Addition to US8 - client now can see price for service and what has been done to vehicle

--- WorkshopManager/Controllers/KlientController.cs
+++ WorkshopManager/Controllers/KlientController.cs
@@ -45,4 +45,61 @@ public class KlientController : Controller
 
         return View(vehicles);
     }
+    
+    // GET: /Klient/OrdersForVehicle?vehicleId=123
+    // Wyświetla listę zleceń przypisanych do danego pojazdu (tylko klient- właściciel pojazdu)
+    public async Task<IActionResult> OrdersForVehicle(int vehicleId)
+    {
+        // 1) Odczyt użytkownika i zabezpieczenie
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+            return Challenge();
+
+        // 2) Sprawdzamy, czy taki pojazd w ogóle istnieje i należy do tego klienta
+        var vehicle = await _context.Vehicles
+            .FirstOrDefaultAsync(v => v.Id == vehicleId && v.ClientId == user.Id);
+        if (vehicle == null)
+            return NotFound(); 
+
+        // 3) Wczytujemy zlecenia dla tego pojazdu, łącznie z podstawowymi danymi (np. status, data, mechanik, liczba czynności)
+        var orders = await _context.ServiceOrders
+            .Where(o => o.VehicleId == vehicleId)
+            .Include(o => o.Mechanic)
+            // jeśli chcemy wyświetlać liczbę czynności:
+            .Include(o => o.ServiceTasks)
+            .ToListAsync();
+
+        ViewBag.VehicleName = $"{vehicle.Brand} {vehicle.Model} (VIN: {vehicle.VIN})";
+        ViewBag.VehicleId = vehicleId;
+        return View(orders);
+    }
+
+    // GET: /Klient/OrderDetails/{orderId}
+    // Szczegóły zlecenia (lista czynności + części) – tylko do odczytu
+    public async Task<IActionResult> OrderDetails(int orderId)
+    {
+        // 1) Pobranie użytkownika
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+            return Challenge();
+
+        // 2) Wczytujemy konkretne zlecenie z wszystkimi nawigacjami:
+        var order = await _context.ServiceOrders
+            .Include(o => o.Vehicle)
+            .Include(o => o.Mechanic)
+            .Include(o => o.ServiceTasks)
+                .ThenInclude(st => st.UsedParts)
+                    .ThenInclude(up => up.Part)
+            .FirstOrDefaultAsync(o => o.Id == orderId);
+
+        if (order == null)
+            return NotFound();
+
+        // 3) Sprawdźmy, czy zlecenie należy do jednego z pojazdów tego klienta
+        if (order.Vehicle.ClientId != user.Id)
+            return Forbid();
+
+        ViewBag.VehicleId = order.VehicleId;
+        return View(order);
+    }
 }
